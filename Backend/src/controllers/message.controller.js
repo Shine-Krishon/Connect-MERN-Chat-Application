f654f03f3a0e37d556a8b89c11f2d1c947ee5b3c
Shine-Krishon <shinekrishon123@gmail.com>
2025-02-15 23:36:17 +0530

    Message route and controller

---
+++ Backend/src/controllers/message.controller.js
@@ -0,0 +1,35 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUser = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUser },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error getting users for sidebar", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.param;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error getting messages", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
