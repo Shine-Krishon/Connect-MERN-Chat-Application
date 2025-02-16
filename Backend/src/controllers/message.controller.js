dc515d5d991a90f56173dcc988208857e0dc9395
Shine-Krishon <shinekrishon123@gmail.com>
2025-02-16 18:42:32 +0530

    backend setup done

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -33,3 +33,34 @@ export const getMessages = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      // Upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId: senderId,
+      receiverId: receiverId,
+      text: text,
+      img: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // TODO: Realtime fumctionality goes here ===> socket.io
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error("Error sending message", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
